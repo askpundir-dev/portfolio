eabc0562251683752600ec94bbf38770f446839b
askpundir-dev <ask.pundir0224@gmail.com>
2025-09-27 20:25:00 +0530

    almost completed the hero-section

--- react-portfolio/src/pages/Home.jsx
+++ react-portfolio/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import LinkedInLogo from "../assets/icons/linkedin.svg?react";
 import GitHubLogo from "../assets/icons/github.svg?react";
 import EmailLogo from "../assets/icons/email.svg?react";
 import NameLogo from "../assets/images/name-logo.svg?react";
+import "./Home.css";
 import { useDaRkModeContext } from "../context-provider/context";
 
 //Home.jsx
@@ -11,16 +12,18 @@ export default function Home() {
   return (
     <>
       <Header />
-      <div className="flex justify-between items-center h-[70vh] px-7">
+      <div className="flex justify-between items-center h-[70vh] px-7 my-4">
         <div></div>
         <div className="flex items-center w-[600px] h-auto relative">
-          <div className="flex flex-col gap-4 absolute items-start">
-            <h1 className="text-[2rem] font-semibold w-max">Abhishek Pundir</h1>
-            <h2 className="">
+          <div className="flex flex-col gap-4 absolute left-[-0.4rem] items-start md:sm:left-[-3rem] lg:left-[-8rem]">
+            <h1 className="text-[1.8rem] lg:text-[2.1rem] font-semibold w-max font-mono">
+              Abhishek Pundir
+            </h1>
+            <p className="font-sans">
               <i>Front-End Developer</i>
-            </h2>
+            </p>
             <button
-              className="text-white bg-blue-600 cursor-pointer px-3 py-1 rounded-[4px] hover:opacity-[0.8]"
+              className="topSectionBtn"
               onClick={() => {
                 console.log("eww bro");
               }}
@@ -73,6 +76,21 @@ export default function Home() {
           <li></li>
         </ul>
       </div>
+      <button
+        className="topSectionBtn mx-auto mt-3 block relative group"
+        onClick={() => {
+          console.log("eww bro");
+        }}
+      >
+        Latest Works
+        <img
+          src="/src/assets/images/downarrow.png"
+          alt="down-arrow"
+          className={`absolute top-9 left-2/6 w-11 md:group-hover:translate-y-4 transition-all duration-400 ease-in-out ${
+            isDarkMode && "invert"
+          } `}
+        />
+      </button>
     </>
   );
 }
