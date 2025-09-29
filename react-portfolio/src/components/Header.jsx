a10980d0ff8a712eeaaa8578a1119c2e7f1d660f
askpundir-dev <ask.pundir0224@gmail.com>
2025-09-29 12:41:05 +0530

    added smooth transitions to icons

--- react-portfolio/src/components/Header.jsx
+++ react-portfolio/src/components/Header.jsx
@@ -3,15 +3,17 @@ import DarkModeIcon from "../assets/icons/moon.svg?react";
 import LightModeIcon from "../assets/icons/sun.svg?react";
 // import PhoneIcon from "../assets/icons/phone.svg?react";
 // import WhatsAppIcon from "../assets/icons/whatsapp.svg?react";
+import EmailLogo from "../assets/icons/email.svg?react";
 import { useDaRkModeContext } from "../context-provider/context";
 import { Link } from "react-router-dom";
 import "./Header.css";
 
 export default function Header() {
   const { isDarkMode, setIsDarkMode } = useDaRkModeContext();
+ const mailLink = import.meta.env.VITE_GMAIL_URL;
 
   const toggleDarkMode = () => setIsDarkMode((prev) => !prev);
-
+  const hoverStyle = isDarkMode ? "hover:text-white" : "hover:border-white";
   return (
     <header
       className={`flex h-[10vh] justify-around items-center  py-3 gap-x-20  
@@ -19,17 +21,34 @@ export default function Header() {
       ${isDarkMode ? "" : "bg-red-800"}`}
     >
       <a href="/">
-        <NameLogo className="nameLogo" />
+        <NameLogo
+          className={`icon nameLogo ${
+            isDarkMode ? "hover:text-white" : "hover:border-white"
+          }`}
+        />
       </a>
-      <div className="flex gap-3" title='Toggle Theme'>
+      <div className="flex gap-3">
         {!isDarkMode && (
-          <DarkModeIcon className="icon" onClick={toggleDarkMode} />
+          <button title="Toggle Dark Mode">
+            <DarkModeIcon
+              className={`icon iconSize ${hoverStyle}`}
+              onClick={toggleDarkMode}
+            />
+          </button>
         )}
         {isDarkMode && (
-          <LightModeIcon className="icon" onClick={toggleDarkMode} />
+          <button title="Toggle Light Mode">
+            <LightModeIcon
+              className="icon iconSize hover:text-white"
+              onClick={toggleDarkMode}
+              title="Toggle Light Mode"
+            />
+          </button>
         )}
-        {/* <WhatsAppIcon className="icon" />
-        <PhoneIcon className="icon" /> */}
+
+        <a href={mailLink}>
+          <EmailLogo className={`icon iconSize ${hoverStyle} `} />
+        </a>
       </div>
     </header>
   );
