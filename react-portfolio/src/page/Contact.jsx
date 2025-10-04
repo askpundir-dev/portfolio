7e21e993a17cf5ed1de9ad6fcf7b888f52ac72bf
askpundir-dev <ask.pundir0224@gmail.com>
2025-10-04 21:07:51 +0530

    improved form svgs

--- react-portfolio/src/page/Contact.jsx
+++ react-portfolio/src/page/Contact.jsx
@@ -78,19 +78,19 @@ export default function Contact({ isDarkMode }) {
                 {/* SVG icon inside the input */}
                 <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
                   <svg
-                    className="w-4 h-4 text-gray-400"
-                    aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
                     fill="none"
-                    viewBox="0 0 20 20"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="w-4 h-4 text-gray-400"
                   >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M10 10a4 4 0 1 0 0-8 4 4 0 0 0 0 8ZM4 18a8 8 0 1 1 12 0H4Z"
-                    />
+                    <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                    <circle cx="12" cy="7" r="4"></circle>
                   </svg>
                 </div>
 
@@ -120,19 +120,14 @@ export default function Contact({ isDarkMode }) {
                 {/* SVG icon inside the input */}
                 <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
                   <svg
-                    className="w-4 h-4 text-gray-400"
+                    className="w-4 h-4 text-gray-500 dark:text-gray-400"
                     aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
+                    fill="currentColor"
                     viewBox="0 0 20 16"
                   >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M1 4.27V12a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V4.27l-8 5.33L1 4.27ZM1 4l9 6 9-6"
-                    />
+                    <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z" />
+                    <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z" />
                   </svg>
                 </div>
 
